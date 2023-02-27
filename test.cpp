b38aabdbd509fc408dc9793b7bf423f93980d695
Verrem98 <emielverhoef@hotmail.com>
2023-02-27 13:27:04 +0100

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,32 @@
+int main()
+{
+    int howMany;
+    double sum;
+    double *a;
+
+    return 0;
+}
+
+/* readArray fills an array with values from a file.
+ * Receive: fileName, a char*,
+ *          a, the address of a pointer to an array,
+ *          n, the address of an int.
+ * PRE: fileName contains N, followed by N double values.
+ * POST: a points to a dynamically allocated array
+ *        containing the N values from fileName
+ *        and n == N.
+ */
+
+void readArray(char *fileName)
+{
+    int count, howMany;
+    double *tempA;
+
+
+}
+
+/* sumArray sums the values in an array of doubles.
+ * Receive: a, a pointer to the head of an array;
+ *          numValues, the number of values in the array.
+ * Return: the sum of the values in the array.
+ */
